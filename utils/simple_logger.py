dd262a70b0590819df513584bfe3ac2880aa52ea
Wesley Zhang <wesleyshsjc@gmail.com>
2023-05-08 01:08:57 -0700

    centralized log level via config file

--- utils/simple_logger.py
+++ utils/simple_logger.py
@@ -1,7 +1,9 @@
 import logging
+import yaml
 
 class Log:
     def __init__(self, name, level=logging.INFO):
+        print(f'Initializing logger with {name} and level {level}')
         self.logger = logging.getLogger(name)
         self.logger.setLevel(level)
 
@@ -34,3 +36,18 @@ class Log:
 
     def critical(self, message):
         self.logger.critical(message)
+
+    @staticmethod
+    def get_logger(self, name, level=logging.INFO):
+        if self.logger is not None:
+            return self.logger
+        else:
+            # Read the config file
+            with open("logging_config.yaml", "r") as yaml_file:
+                config = yaml.safe_load(yaml_file)
+
+            # Get the logging level from the config file
+            log_level_str = config.get("logging", {}).get("level", "INFO")
+            log_level = getattr(logging, log_level_str.upper(), logging.INFO)
+
+            return Log(name, level)
