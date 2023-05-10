db228015e81a04b0a45991d3bb5b28e29122cff2
Wesley Zhang <wesleyshsjc@gmail.com>
2023-05-10 12:14:56 -0700

    add streaming use case

--- doc_search_llm/llm_doc_streaming_query.py
+++ doc_search_llm/llm_doc_streaming_query.py
@@ -22,12 +22,42 @@ chain = None
 docs = None
 
 async def websocket_handler(request):
+    log.info("New connection")
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            query = msg.data
+            log.debug(f"Received query: {query}")
+            vectorstore_processor = request.app['vectorstore_processor']
+            docs = vectorstore_processor.vectorstore.similarity_search(query)
+            if docs is not None:
+                log.info(f'best matched docs count: {len(docs)}')
+                log.debug(f'Best matched docs: {docs[0]}')
+
+                chain = request.app['chain']
+                answer = chain.run(input_documents=docs, question=query)
+
+                await ws.send_str(str(answer))
+            else:
+                await ws.send_str("No answer found in the knowledge base")
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            log.error('WebSocket connection closed with exception %s' % ws.exception())
+
+    log.info('WebSocket connection closed')
+    return ws
+
+async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
 
     async for msg in ws:
         if msg.type == aiohttp.WSMsgType.TEXT:
             log.info(f'query: {msg.data}')
+            chain = request.app['chain']
+            result = chain.run(input_documents=docs, question=query)
+
             await ws.send_str(chain.run(input_documents=docs, question=msg.data))
         elif msg.type == aiohttp.WSMsgType.ERROR:
             print('WebSocket connection closed with exception %s' %
@@ -39,44 +69,29 @@ async def websocket_handler(request):
 app = web.Application()
 app.router.add_get('/ws', websocket_handler)
 
-def main(args):
 
-    # load the documents from the docs directory
+async def on_startup(app):
+    args = app['args']
+
     directory_processor = DirectoryProcessor(
         docs_root=args.docs_root, global_kwargs=args.global_kwargs)
     docs = directory_processor.load(
         chunk_size=args.chunk_size, chunk_overlap=args.chunk_overlap)
 
-    # convert the documents to vectorstore
     vectorstore_processor = VectorstoreProcessor()
     vectorstore_processor.convert_from_docs(docs)
+    app['vectorstore_processor'] = vectorstore_processor
 
-    if args.query is not None:
-        # get the query string
-        query = args.query
+    # load the LLM model
+    model, tokenizer = ModelProcessor.load_model(args)
 
-        # load the LLM model
-        model, tokenizer = ModelProcessor.load_model(args)
-
-        # create the LLM pipeline
-        pipe = pipeline("text-generation", model=model,
-                        tokenizer=tokenizer, max_new_tokens=1024)
-        llm = HuggingFacePipeline(pipeline=pipe)
-
-        # load the QA chain
-        chain = load_qa_chain(llm, chain_type="stuff")
-
-        # search top N best matched documents to reduce the scope
-        docs = vectorstore_processor.vectorstore.similarity_search(
-            query, args.doc_count_for_qa, include_metadata=True)
-        log.info(f'similarity searched {len(docs)} documents')
-
-        # run the LLM query by feeding the best matched documents
-        result = chain.run(input_documents=docs, question=query)
-        log.info(f'LLM query: {query}')
-        log.info(f'LLM result: {result}')
-    else:
-        log.error(f'no query string provided, please provide a query string by using --query')
+    # create the LLM pipeline
+    pipe = pipeline("text-generation", model=model,
+                    tokenizer=tokenizer, max_new_tokens=1024)
+    llm = HuggingFacePipeline(pipeline=pipe)
+    # load the QA chain
+    chain = load_qa_chain(llm, chain_type="stuff")
+    app['chain'] = chain
 
 if __name__ == "__main__":
 
@@ -100,9 +115,18 @@ if __name__ == "__main__":
     parser.add_argument(
         '--load_in_8bit', action='store_true', help='Load in 8 bits')
     parser.add_argument('--bf16', action='store_true', help='Use bf16')
-    parser.add_argument('--doc_count_for_qa', type=int, default=4,  help='doc count for QA')
+    parser.add_argument('--doc_count_for_qa', type=int,
+                        default=4,  help='doc count for QA')
     args = parser.parse_args()
     log.info(f'args: {args}')
 
-    main(args)
+    # Create app and register on_startup function and route
+    app = web.Application()
+    app.on_startup.append(on_startup)
+    app.router.add_get('/ws', websocket_handler)
+
+    # Store args in the app object
+    app['args'] = args
+
+    # Run the app
     web.run_app(app, port=5000)
