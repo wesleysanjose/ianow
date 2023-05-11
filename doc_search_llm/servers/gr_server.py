a4675370acd393c3d9199c89f68fa3873c076b28
Wesley Zhang <wesleyshsjc@gmail.com>
2023-05-11 14:21:42 -0700

    remove unnecessary args from gr_server

--- doc_search_llm/servers/gr_server.py
+++ doc_search_llm/servers/gr_server.py
@@ -1,6 +1,5 @@
 import gradio as gr
-from .arg_parser import load_parser
-
+import argparse
 import sys
 from pathlib import Path
 
@@ -36,7 +35,26 @@ with gr.Blocks() as demo:
 
 if __name__ == "__main__":
 
-    args = load_parser()
+    # process command line arguments
+    parser = argparse.ArgumentParser(description='Process the arguments.')
+    parser.add_argument('--modle_name_or_path', type=str,
+                        help='model name or path for LLM query')
+    parser.add_argument('--chunk_size', type=int, default=1000,
+                        help='chunk size (default: 1000)')
+    parser.add_argument('--chunk_overlap', type=int,
+                        default=100, help='chunk overlap (default: 100)')
+    parser.add_argument('--global_kwargs', type=str,
+                        default="**/*.txt", help='global kwargs (default: **/*.txt')
+    parser.add_argument('--persist_directory', type=str, default="chroma_storage",
+                        help='persist directory (default: chroma_storage')
+    parser.add_argument(
+        '--load_in_8bit', action='store_true', help='Load in 8 bits')
+    parser.add_argument('--bf16', action='store_true', help='Use bf16')
+    parser.add_argument('--doc_count_for_qa', type=int,
+                        default=4,  help='doc count for QA')
+    parser.add_argument('--port', type=int, default=5000, help='port number')
+
+    args = parser.parse_args()
     log.info(f'args: {args}')
 
     demo.launch(server_name="0.0.0.0")
