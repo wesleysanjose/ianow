f51b9c75e575be032b0ae1bbdaa6210c2ce31926
Wesley Zhang <wesleyshsjc@gmail.com>
2023-05-11 09:38:54 -0700

    gr server expose at 0.0.0.0

--- doc_search_llm/servers/gr_server.py
+++ doc_search_llm/servers/gr_server.py
@@ -5,4 +5,4 @@ def process_file(file):
 
 iface = gr.Interface(fn=process_file, inputs="file", outputs="text")
 
-iface.launch(share=True)  # share=True will generate a link for you to share with others))
+iface.launch(share=True, server_name="0.0.0.0")  # share=True will generate a link for you to share with others))
