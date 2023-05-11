c5916a634c22e77b2627f9161208ad0da3203397
Wesley Zhang <wesleyshsjc@gmail.com>
2023-05-11 15:15:05 -0700

    ready for git store

--- doc_search_llm/servers/gr_server.py
+++ doc_search_llm/servers/gr_server.py
@@ -1,40 +1,33 @@
+from utils.simple_logger import Log
 import gradio as gr
 import argparse
 import sys
 from pathlib import Path
+from doc_search_llm.modules.chroma_processor import ChromaProcessor
+from doc_search_llm.modules.directory_processor import DirectoryProcessor
+from doc_search_llm.modules.model_processor import ModelProcessor
+from langchain.chains.question_answering import load_qa_chain
+from transformers import pipeline
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 
 sys.path.append(str(Path(__file__).parent.parent.absolute()))
-from utils.simple_logger import Log
 log = Log.get_logger(__name__)
 
-# show the file content in the text box
-# show the chatbot response in the text box
-with gr.Blocks() as demo:
+
+def load_model(args):
+    # load the LLM model
+    model, tokenizer = ModelProcessor.load_model(args)
+
+    # create the LLM pipeline
+    pipe = pipeline("text-generation", model=model,
+                    tokenizer=tokenizer, max_new_tokens=1024)
+    llm = HuggingFacePipeline(pipeline=pipe)
+    # load the QA chain
+    chain = load_qa_chain(llm, chain_type="stuff")
+    return chain
 
 
-    # file_input = gr.File(label="Upload File")
-    # output = gr.Markdown()
-    def process_file(file):
-        with open(file.name, encoding="utf-8") as f:
-            content = f.read()
-            return content
-    
-    gr.Interface(fn=process_file, inputs="file", outputs="text")
-
-    chatbot = gr.Chatbot()
-    msg = gr.Textbox()
-    clear = gr.Button("Clear")
-
-    def respond(message, chat_history):
-        bot_message = "hello"
-        chat_history.append((message, bot_message))
-        return "", chat_history
-
-    msg.submit(respond, [msg, chatbot], [msg, chatbot])
-    clear.click(lambda: None, None, chatbot, queue=False)
-
-if __name__ == "__main__":
-
+def load_args():
     # process command line arguments
     parser = argparse.ArgumentParser(description='Process the arguments.')
     parser.add_argument('--modle_name_or_path', type=str,
@@ -56,5 +49,62 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     log.info(f'args: {args}')
+
+    return args
+
+
+def load_docs(args):
+    directory_processor = DirectoryProcessor(
+        docs_root=args.docs_root, global_kwargs=args.global_kwargs)
+    docs = directory_processor.load(
+        chunk_size=args.chunk_size, chunk_overlap=args.chunk_overlap)
+
+    vectorstore_processor.convert_from_docs(docs)
+    return vectorstore_processor
+
+def query_to_llm(vectorstore_processor, chain, query):
+    results = vectorstore_processor.vectorstore.similarity_search(query)
+    docs = vectorstore_processor.vectorstore.similarity_search(query)
+    if docs is not None:
+        log.info(f'best matched docs count: {len(docs)}')
+        log.debug(f'Best matched docs: {docs[0]}')
+
+        answer = chain.run(input_documents=docs, question=query)
+    return answer
+
+
+if __name__ == "__main__":
+
+    args = load_args()
+
+    chain = load_model(args)
+
+    vectorstore_processor = ChromaProcessor()
+
+    # show the file content in the text box
+    # show the chatbot response in the text box
+    with gr.Blocks() as demo:
+
+        # file_input = gr.File(label="Upload File")
+        # output = gr.Markdown()
+        def process_file(file):
+            with open(file.name, encoding="utf-8") as f:
+                content = f.read()
+                vectorstore_processor.convert_from_docs(content)
+                return content
+
+        gr.Interface(fn=process_file, inputs="file", outputs="text")
+
+        chatbot = gr.Chatbot()
+        msg = gr.Textbox()
+        clear = gr.Button("Clear")
+
+        def respond(message, chat_history):
+            bot_message = query_to_llm(vectorstore_processor, chain, message)
+            chat_history.append((message, bot_message))
+            return "", chat_history
+
+        msg.submit(respond, [msg, chatbot], [msg, chatbot])
+        clear.click(lambda: None, None, chatbot, queue=False)
 
     demo.launch(server_name="0.0.0.0")
