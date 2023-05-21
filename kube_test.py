0a8776da78194baf6b58fe378ce9bde566c2b265
Wesley Zhang <wesleyshsjc@gmail.com>
2023-05-20 20:34:47 -0700

    change it back

--- kube_test.py
+++ kube_test.py
@@ -35,8 +35,9 @@ def main():
     print("Listing pods with their IPs:")
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
-        #print("%s\t%s\t%s" %
-        #      (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-        print(i)
+        print("%s\t%s\t%s" %
+              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        #print(i)
+
 if __name__ == '__main__':
     main()
