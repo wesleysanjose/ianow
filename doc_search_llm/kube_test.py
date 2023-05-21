1f3f8ef50573c95ddeb51ff7785de6bde3bf33ac
Wesley Zhang <wesleyshsjc@gmail.com>
2023-05-20 20:15:20 -0700

    bug fixing

--- doc_search_llm/kube_test.py
+++ doc_search_llm/kube_test.py
@@ -14,18 +14,19 @@ def main():
     configuration.api_key["authorization"] = f'Bearer {token}'
     configuration.api_key_prefix['authorization'] = 'Bearer'
 
+    configuration.verify_ssl = False
+
     # Set the created configuration as default
-    client.Configuration.set_default(configuration)
+    #client.Configuration.set_default(configuration)
+    client = client.ApiClient(configuration)
 
-    v1 = client.CoreV1Api()
+    v1 = client.CoreV1Api(client)
 
-    print("Listing services with their info:\n")
-    services = v1.list_namespaced_service(namespace="default")  # Change 'default' to your namespace
-    for svc in services.items:
-        print(f"Name: {svc.metadata.name}")
-        print(f"Labels: {svc.metadata.labels}")
-        print(f"Cluster IP: {svc.spec.cluster_ip}")
-        print(f"Ports: {svc.spec.ports}\n")
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" %
+              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
 if __name__ == '__main__':
     main()
