bfe55c0aa2a6762d7f755c6336c37772fdd9eece
Wesley Zhang <wesleyshsjc@gmail.com>
2023-05-20 20:33:27 -0700

    change log print format

--- kube_test.py
+++ kube_test.py
@@ -35,8 +35,8 @@ def main():
     print("Listing pods with their IPs:")
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-
+        #print("%s\t%s\t%s" %
+        #      (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        print(i)
 if __name__ == '__main__':
     main()
