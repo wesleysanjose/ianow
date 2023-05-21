23869568f1e1642671f3cc0ba39cb562edbfcec0
Wesley Zhang <wesleyshsjc@gmail.com>
2023-05-21 07:05:08 -0700

    bug fix

--- kube_test.py
+++ kube_test.py
@@ -74,7 +74,8 @@ def load_parser():
 def dump_json(data):
     try:
         with open("output.json","w") as write_file:
-            json.dump([d for d in data], write_file)
+            for d in data:
+                json.dump(d, write_file)
             write_file.close()
         
         flattened = [flatten(d) for d in data]
